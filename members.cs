ee9bc5b25ecf62c094cf17168ad09b07a4b46003
kavitharaja31 <110021926+kavitharaja31@users.noreply.github.com>
2022-08-22 13:42:34 +0530

    Create members.cs

---
+++ members.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Reflection;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    internal class Prgrm
+    {
+        static void FieldInvestigation(Type T)
+        {
+            Console.WriteLine("Fields");
+            FieldInfo[] fieldInfos = T.GetFields();
+            foreach (FieldInfo fieldInfo in fieldInfos)
+            {
+                Console.WriteLine(fieldInfo.Name);
+            }
+        }
+        static void MethodInvestigation(Type T)
+        {
+            Console.WriteLine("Methods");
+            MethodInfo[] methodInfos = T.GetMethods();
+            foreach (MethodInfo methodInfo in methodInfos)
+            {
+                Console.WriteLine(methodInfo.Name);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the explore name");
+            string typName = Console.ReadLine();
+            Type T = Type.GetType(typName);
+            FieldInvestigation(T);
+            MethodInvestigation(T);
+            Console.ReadKey();
+        }
+    }
+}
