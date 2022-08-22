4448d5bd5533d8b19a68d9cbadc5fd70e135e182
kavitharaja31 <110021926+kavitharaja31@users.noreply.github.com>
2022-08-22 13:24:29 +0530

    Create FileIO.cs

---
+++ FileIO.cs
@@ -0,0 +1,54 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIO_Assignment
+{
+    class Prgm
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string text = System.IO.File.ReadAllText(@"C:\test\text files\kavi.txt");
+                System.Console.WriteLine("Contents of text file = {0} ", text);
+                string[] lines = System.IO.File.ReadAllLines("C:\test\text files\kavi.txt");
+                System.Console.WriteLine("Contents of text file2 = ");
+                foreach (string line in lines)
+                {
+                    Console.WriteLine("\t " + line);
+                }
+                Console.WriteLine("press any key to exit.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            string rootdir = @"C:\test\text files";
+            string[] files = Directory.GetFiles(rootdir);
+            Console.WriteLine(string.Join(Environment.NewLine, files));
+            string[] dirs = Directory.GetDirectories(rootdir);
+            string[] subdir = Directory.GetDirectories(rootdir);
+            Console.WriteLine(string.Join(Environment.NewLine, dirs));
+            DirectoryInfo directoryInfo = new DirectoryInfo(rootdir);
+            if (directoryInfo.Exists)
+            {
+                Console.WriteLine("Directory already exist");
+            }
+            directoryInfo.Create();
+            Console.WriteLine("Directory created");
+            FileInfo fileinfo = new FileInfo(@"C:\test\text files\kavi.txt");
+            StreamWriter streamWriter = fileinfo.CreateText();
+            streamWriter.WriteLine("file created");
+            streamWriter.WriteLine("welcome to c#");
+            streamWriter.Close();
+            StreamReader streamReader = fileinfo.OpenText();
+            Console.WriteLine(streamReader.ReadToEnd());
+            streamReader.Close();
+            System.Console.ReadKey();
+        }
+    }
+}
